5230b887f1dd0968ce63e2ec7d1588d467b1b1eb
zharif <zharif@ig-interactive.com>
2018-05-06 01:21:00 +0800

    Initial Commit

---
+++ SearchController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchController
+//
+//  Created by M. Zharif Hadi M. Khairuddin on 06/05/2018.
+//  Copyright © 2018 M. Zharif Hadi M. Khairuddin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
